c5569de302015721b98e8f477532032f3b9b71a7
Kauna Mohammed <kaunamohammed@gmail.com>
2020-06-08 09:11:45 +0100

    fixing merge conflicts

--- OkraWidget/OkraWidget/OkraWebView.swift
+++ OkraWidget/OkraWidget/OkraWebView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class OkraWebView: UIViewController, WKScriptMessageHandler {
+class OkraWebView: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
     
     public var okraOptions: OkraOptions!
     
@@ -24,83 +24,51 @@ class OkraWebView: UIViewController, WKScriptMessageHandler {
         super.loadView()
         web.configuration.userContentController.add(self, name: "jsMessageHandler")
         web.configuration.userContentController.add(self, name: "jsErrorMessageHandler")
+        web.configuration.userContentController.add(self, name: "jsCloseMessageHandler")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        linkOptions = [
-            "isWebview": String(okraOptions.isWebview),
-            "key": okraOptions.key,
-            "token": okraOptions.token,
-            "source": "ios",
-            "products": convertProductArrayToString(productList: okraOptions.products),
-            "env": okraOptions.env,
-            "clientName": okraOptions.clientName,
-            "webhook": "http://requestb.in",
-            "baseUrl": "https://demo-dev.okra.ng/link.html"
-        ]
-        
-        
-        let request = URLRequest(url: formatUrl(options: linkOptions))
+        let request = URLRequest(url: formatUrl())
         web.load(request)
-        web.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
+        web.navigationDelegate = self
     }
     
-
-    // Observe value
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if let _ = change?[NSKeyValueChangeKey.newKey] {
-            switchToPreviousPage();
-        }
+    func webView(_ webView: WKWebView, didFinish  navigation: WKNavigation!)
+    {
+        do{
+            let okraOptionsEncoded = okraOptions.encode()
+            let jsonEncoder = JSONEncoder()
+            let jsonData = try jsonEncoder.encode(okraOptionsEncoded)
+            let json = String(data: jsonData, encoding: String.Encoding.utf8)
+            web.evaluateJavaScript("openOkraWidget('"+json!+"')", completionHandler: { (object,error) in})
+        }catch{}
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if(message.name == "jsMessageHandler") {
-            OkraHandler.data = message.body as! String;
-            OkraHandler.isSuccessful = true;
-            OkraHandler.isDone = true;
-            print(OkraHandler.isDone)
-            print(OkraHandler.data)
-            switchToPreviousPage();
-        } else {
-            print(message.name)
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "okra.onSuccess"), object: ["data": message.body as! String])
+            
+            
+        }else if(message.name == "jsErrorMessageHandler"){
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "okra.onError"), object: ["data": message.body as! String])
+            
+        }else if(message.name == "jsCloseMessageHandler"){
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "okra.onClose"), object: ["data": message.body as! String])
         }
+        switchToPreviousPage();
     }
     
-    func switchToPreviousPage() {
-        dismiss(animated: true, completion: nil)
+    func switchToPreviousPage(){
+        self.dismiss(animated: true, completion: nil)
     }
     
-    func convertProductArrayToString(productList: [String]) -> String {
-        
-        var formattedArray = "[";
-        
-        for (index, name) in productList.enumerated(){
-            if(index == (productList.count - 1)){
-                formattedArray.append("\"\(name)\"")
-            }else{
-                formattedArray.append("\"\(name)\",")
-            }
-        }
-        formattedArray.append("]")
-        
-        return formattedArray;
-    }
-    
-    
-    func formatUrl(options: [String: String]) -> URL {
-        
-        
+    func formatUrl() -> URL{
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
-        urlComponents.host = "app.okra.ng"
-        //urlComponents.port = 3000
+        urlComponents.host = "mobile.okra.ng"
         urlComponents.path = "/"
-        urlComponents.queryItems = options
-            .filter { (item) in item.key != "baseUrl" || item.key != "webhook" }
-            .map { (key, value) in URLQueryItem(name: key, value: value) }
-        
         return urlComponents.url!;
     }
     
