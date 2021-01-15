cd09aaab6d206186b77358f53182db558e4b62cc
Bolu Okunaiya <Boluokunaiya@gmail.com>
2021-01-15 08:48:07 +0100

    Initial commit

---
+++ Example/OkraWidget/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OkraWidget
+//
+//  Created by boluok on 01/15/2021.
+//  Copyright (c) 2021 boluok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
